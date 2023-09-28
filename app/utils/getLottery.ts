45b20fede41994baea1acc28868286ccda93cc29
Keyur Patel <ekeyur@gmail.com>
2023-09-27 20:10:11 -0400

    removed w-full

--- app/utils/getLottery.ts
+++ app/utils/getLottery.ts
@@ -23,7 +23,6 @@ await doc.loadInfo(); // loads document properties and worksheets
 
 const sheet = doc.sheetsByTitle[sheetname];
 const sheetRows = await sheet.getRows();
-
 const data = sheetRows.map(row => row.toObject());
 
 const img_width = data[0].image_width;
@@ -34,22 +33,23 @@ const state = data[0].state;
 
 const moreData = await Promise.allSettled(data.map(async each_row => {
  
-  async function getData(url:string) {
-    const res = await fetch(url);
-    return await res.json(); 
-  }
+  // async function getData(url:string) {
+  //   const res = await fetch(url);
+  //   return await res.json(); 
+  // }
 
 
 // @ts-ignore
-  const lotdata = await getData(getDataUrl(each_row.game_number, state));
+  // const lotdata = await getData(getDataUrl(each_row.game_number, state));
+  // console.log(lotdata);
 
   return {
     slot_number: each_row.slot_number,
     game_number: each_row.game_number,
-    image_url: getImageUrl(each_row.game_number, state),
-    data_url:  each_row.data_url,
-    game_name: lotdata.gameName,
-    ticket_price: lotdata.ticketPrice,
+    image_url: each_row.image_url,
+    game_name: each_row.game_name,
+    ticket_price: each_row.ticketPrice,
+    is_featured: each_row.is_featured
   }
 }));
 
