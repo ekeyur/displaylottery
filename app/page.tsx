7e6262349dae52ec682108f244e709ac8f57abf9
Keyur Patel <ekeyur@gmail.com>
2023-10-21 21:05:52 -0400

    added featured feature

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex h-screen justify-between">
+    <main className="flex h-screen justify-between bg-white">
       <div className="z-10 w-full h-full flex items-center justify-center font-mono">
         support@displaylottery.com
       </div>
